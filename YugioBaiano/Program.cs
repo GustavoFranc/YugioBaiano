c91a9f80d31512e3dbd205e78e273f4139b5cea7
Gustavo <gustavofrancisco15@gmail.com>
2024-06-09 11:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ YugioBaiano/Program.cs
@@ -0,0 +1,47 @@
+﻿class Jogador
+{
+    public string Nome { get; set; }
+    public int Vida { get; set; }
+    
+    public Jogador(string nome, int vida)
+    {
+        Nome = nome;
+        Vida = vida;
+    }
+    
+
+}
+
+class Carta
+{
+    public string Nome { get; set; }
+    
+    public Carta(string nome)
+    {
+        Nome = nome;
+    }
+}
+
+class CartaDeMonstro : Carta
+{
+    public int PoderDeAtaque { get; set; }
+    public int PoderDeDefesa { get; set; }
+
+    public CartaDeMonstro(string nome, int poderDeAtaque, int poderDeDefesa) : base(nome) 
+    {
+        Nome = nome;
+        PoderDeAtaque = poderDeAtaque;
+        PoderDeDefesa = poderDeDefesa;
+    }
+
+    public int Atacar()
+    {
+        return PoderDeAtaque;
+    }
+
+    public int Defender()
+    {
+        return PoderDeDefesa;
+    }
+
+}
